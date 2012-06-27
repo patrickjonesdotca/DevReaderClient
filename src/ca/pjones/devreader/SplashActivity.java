945cd978274cf3d4aa7f51bb9a1d2e7011592dbf
Patrick Jones <patrick@patrickjones.ca>
2012-06-27 00:29:00 -0400

    Splash Screen now clickable through to main activity

--- src/ca/pjones/devreader/SplashActivity.java
+++ src/ca/pjones/devreader/SplashActivity.java
@@ -9,7 +9,7 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
     }
     
     public void launchApp(View v) {
